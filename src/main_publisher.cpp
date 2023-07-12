39c3cf9029c7181aeac6016905bf1c1ffbc84906
YundoChoi <rikochet@kakao.com>
2023-07-13 00:40:43 +0900

    Update main_publisher.cpp

--- src/main_publisher.cpp
+++ src/main_publisher.cpp
@@ -269,7 +269,7 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Racecar>() );
   rclcpp::shutdown();
-  finally();
+  //finally();
  
   return 0;
 }
